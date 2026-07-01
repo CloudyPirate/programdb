c3bbac1fde3337fb0092f641e4f9936c3a2d3c04
CloudyPirate <bohorquezjuanfer@gmail.com>
2026-06-30 21:46:40 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-uv build
+pip install -r requirements.txt
 
 # Convert static asset files
-python uv run manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-uv run manage.py migrate
-uv run manage.py populate_db
+python manage.py migrate
+python manage.py populate_db
